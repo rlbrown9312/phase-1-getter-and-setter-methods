959ede120e79b462df9c1b0b44c9dd99dd7c9e61
Richard <richard.brown0807@gmail.com>
2022-11-06 03:07:38 -0600

    commit tree e354e8ba101606529d09a1464165e2e61f0889a4

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-// Add your Circle class here
+class Circle {
+    //#radius
+    // #diameter
+    // #circumference
+    // #area
+
+    constructor(radius) {
+        this.radius = radius //* Math.PI;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    get circumference() {
+        return (this.radius * 2) * Math.PI;
+    }
+
+    get area() {
+        return this.radius**2 * Math.PI; 
+    }
+
+    set diameter(diameter) {
+        return this.radius = diameter
+    }
+
+    // set circumference(circumference) {
+    //     this.#circumference = circumference
+    // }
+    
+    // set area(area) {
+    //     this.#area = area
+    // }
+}
+
+const circle = new Circle(6)
+console.log(circle)
+console.log(circle.diameter)
+console.log(circle.circumference)
+console.log(circle.area)
+console.log("-----------------------------------------")
+console.log(circle.radius)
+// console.log(circle.diameter)
+// console.log(circle.circumference)
+// console.log(circle.area)
+
+
+
+
+
+
+
+
+
+
+
+
+// class Circle {
+//     //#radius
+//     // #diameter
+//     // #circumference
+//     // #area
+
+//     constructor(radius) {
+//         this.#radius = radius //* Math.PI;
+//     }
+
+//     get diameter() {
+//         return this.#radius * 2;
+//     }
+
+//     get circumference() {
+//         return this.#radius**2 * Math.PI;
+//     }
+
+//     get area() {
+//         return this.#radius**2 //* Math.PI; 
+//     }
+
+//     set diameter(diameter) {
+//         this.#diameter = diameter
+//     }
+
+//     set circumference(circumference) {
+//         this.#circumference = circumference
+//     }
+    
+//     set area(area) {
+//         this.#area = area
+//     }
+//}
